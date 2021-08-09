6b72fd27540d7c1a36dc9177baad6bd5fce868be
serega1ua <32671836+serega1ua@users.noreply.github.com>
2021-08-09 16:53:06 +0300

    commit without stage

--- 3.js
+++ 3.js
@@ -1,4 +1,4 @@
 function greatestProduct(input) {
-    let arr = input.split``.map((x,i,a) => a.length - i < 5 ? -Infinity : x * a[i+1] * a[i+2] * a[i+3]* a[i+4]);
+    var arr = input.split``.map((x,i,a) => a.length - i < 5 ? -Infinity : x * a[i+1] * a[i+2] * a[i+3]* a[i+4]);
     return   Math.max(...arr);
 }
